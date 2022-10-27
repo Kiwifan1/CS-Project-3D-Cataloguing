26797d1cf8b126cc5112cbb49e705637b729f5bf
Kiwifan1 <joshua.venable@outlook.com>
2022-10-27 11:55:43 -0700

    redid sql

--- Data/Item.sql
+++ Data/Item.sql
@@ -1,1 +1,64 @@
--- Database holding 3D Asset Information
+
+-- Holds the attribute information, such that an asset can have multiple attributes
+CREATE TABLE Attribute
+(
+    Name VARCHAR(50) NOT NULL,
+    Description TINYTEXT,
+    PRIMARY KEY (Name)
+)
+
+-- Each set must belong to a group
+CREATE TABLE AssetGroup
+(
+    Name VARCHAR(50) NOT NULL,
+    Description TINYTEXT,
+    PRIMARY KEY (Name)
+)
+
+-- Each Asset must belong to a set
+CREATE TABLE AssetSet
+(
+    Name VARCHAR(50) NOT NULL,
+    GroupName VARCHAR(50) NOT NULL,
+    Description TINYTEXT,
+    PRIMARY KEY (Name, GroupName),
+    FOREIGN KEY (GroupName) REFERENCES AssetGroup(Name)
+)
+
+-- All asset files must have come from a release of some kind 
+CREATE TABLE Release
+(
+    ID INT NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    Date DATETIME NOT NULL,
+    Publisher VARCHAR(50) NOT NULL,
+    Source VARCHAR(50) NOT NULL,
+    PRIMARY KEY (ID)
+)
+
+-- The metadata of the asset, including it's file path and download date
+CREATE TABLE AssetFile
+(
+    ID INT NOT NULL,
+    ReleaseID INT NOT NULL,
+    Path VARCHAR(100) NOT NULL,
+    Image VARBINARY(MAX) NOT NULL,
+    DownloadDate DATETIME NOT NULL,
+    EditDate DATETIME CHECK (EditDate >= DownloadDate OR EditDate IS NULL),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (ReleaseID) REFERENCES Release(ID)
+)
+
+-- The actual asset, with information about it's name, attributes, and scale
+CREATE TABLE Asset
+(
+    FileID INT NOT NULL,
+    AttributeName VARCHAR(50) NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    SetName VARCHAR(5) NOT NULL,
+    Scale VARCHAR(10) NOT NULL,
+    -- TODO: Fix format for scale
+    PRIMARY KEY (FileID, AttributeName),
+    FOREIGN KEY (FileID) REFERENCES AssetFile(ID),
+    FOREIGN KEY (AttributeName) REFERENCES Attribute(Name),
+)
--- Logic/Logic.java
+++ Logic/Logic.java
@@ -34,5 +34,170 @@ public class Logic {
         }
     }
 
+    /**
+     * Adds an attribute to the database
+     * 
+     * @param id   The id of the attribute
+     * @param name The name of the attribute
+     * @return Returns true if the attribute was added successfully
+     */
+    public boolean addAttribute(int id, String name) {
+        try {
+            String query = "INSERT INTO Attribute VALUES (?, ?)";
+            PreparedStatement ps = cn.prepareStatement(query);
+
+            ps.setInt(1, id);
+            ps.setString(2, name);
+
+            ps.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    /**
+     * Adds a group to the database
+     * 
+     * @param id   The id of the group
+     * @param name The name of the group
+     * @return Returns true if the group was added successfully
+     */
+    public boolean addGroup(int id, String name) {
+        try {
+            String query = "INSERT INTO Groups VALUES (?, ?)";
+            PreparedStatement ps = cn.prepareStatement(query);
+
+            ps.setInt(1, id);
+            ps.setString(2, name);
+
+            ps.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    /**
+     * Adds a set to the database. This method relies upon a group already existing,
+     * one is added by calling {@link #addGroup(int, String)}
+     * 
+     * @param id   The id of the set
+     * @param name The name of the set
+     * @param gid  The id of the group the set belongs to
+     * @return Returns true if the set was added successfully
+     */
+    public boolean addSet(int id, String name, int gid) {
+        try {
+            String query = "INSERT INTO Set VALUES (?, ?, ?)";
+            PreparedStatement ps = cn.prepareStatement(query);
+
+            ps.setInt(1, id);
+            ps.setString(2, name);
+            ps.setInt(3, gid);
+
+            ps.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    /**
+     * Adds a release to the database.
+     * 
+     * @param id   The id of the release
+     * @param name The name of the release
+     * @param date The date of the release
+     * @param pub  The publisher of the release
+     * @param src  The source of the release
+     * @return Returns true if the release was added successfully
+     */
+    public boolean addRelease(int id, String name, String date, String pub, String src) {
+        try {
+            String query = "INSERT INTO Release VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement ps = cn.prepareStatement(query);
+
+            ps.setInt(1, id);
+            ps.setString(2, name);
+            ps.setString(3, date);
+            ps.setString(4, pub);
+            ps.setString(5, src);
+
+            ps.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    /**
+     * Adds a file to the database. This method relies that the release
+     * already exists in the database, one is added by calling
+     * {@link #addRelease(int, String, String, String, String)}
+     * 
+     * @param id           The id of the file
+     * @param rid          The release id of the file
+     * @param path         The path of the file
+     * @param image        The image of the file
+     * @param downloadDate The download date of the file
+     * @param editDate     The edit date of the file
+     * @return Returns true if the file was added successfully
+     */
+    public boolean addFile(int id, int rid, String path, byte[] image, String downloadDate, String editDate) {
+        try {
+            String query = "INSERT INTO AssetFile VALUES (?, ?, ?, ?, ?, ?)";
+            PreparedStatement ps = cn.prepareStatement(query);
+
+            ps.setInt(1, id);
+            ps.setInt(2, rid);
+            ps.setString(3, path);
+            ps.setBytes(4, image);
+            ps.setString(5, downloadDate);
+            ps.setString(6, editDate);
+
+            ps.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    /**
+     * Adds an asset to the database. This method relies that the file was already
+     * added with
+     * {@link #addFile(int, int, String, byte[], String, String)} and it has an
+     * attribute from {@link #addAttribute(int, String)}
+     * 
+     * @param fid   The file ID of the asset
+     * @param aid   The asset ID of the asset
+     * @param name  The name of the asset
+     * @param sid   The set ID of the asset
+     * @param scale The scale of the asset
+     * @return Returns true if the asset was added successfully
+     */
+    public boolean addAsset(int fid, int aid, String name, int sid, String scale) {
+        try {
+            String query = "INSERT INTO Asset VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement ps = cn.prepareStatement(query);
+
+            ps.setInt(1, fid);
+            ps.setInt(2, aid);
+            ps.setString(3, name);
+            ps.setInt(4, sid);
+            ps.setString(5, scale);
+
+            ps.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
 
 }
