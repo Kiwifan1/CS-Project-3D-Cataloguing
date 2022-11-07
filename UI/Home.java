57fd9f6ed61627e00e78d78daa87bc1b91c72724
Kiwifan1 <joshua.venable@outlook.com>
2022-11-07 15:33:12 -0800

    centered text

--- UI/Home.java
+++ UI/Home.java
@@ -1,10 +1,18 @@
 package UI;
 
-import javax.swing.*;
-
 import Logic.ConnectLogic;
+import java.util.List;
+import java.io.*;
 
-import java.awt.*;
+import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+
+import java.awt.dnd.*;
+import java.awt.datatransfer.*;
+import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Font;
 
 public class Home extends JFrame {
 
@@ -13,30 +21,84 @@ public class Home extends JFrame {
 
     ConnectLogic logic;
 
+    JPanel homePanel;
     JPanel cataloguePanel;
+    JTextPane dragArea;
     JButton catalogueBtn;
 
     public Home() {
         super("Home");
+        // logic = new ConnectLogic();
+
+        // set up the frame
         setSize(WIDTH, HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        logic = new ConnectLogic();
-
-        makeCataloguePanel();
+        // populate the frame
+        createDragArea();
+        makeHomePanel();
 
         this.setVisible(true);
     }
 
+    private void makeHomePanel() {
+        homePanel = new JPanel(new BorderLayout());
+
+        makeCataloguePanel();
+
+        homePanel.add(cataloguePanel, BorderLayout.CENTER);
+
+        this.add(homePanel, BorderLayout.CENTER);
+    }
+
     private void makeCataloguePanel() {
         cataloguePanel = new JPanel();
+        cataloguePanel.setLayout(new BoxLayout(cataloguePanel, BoxLayout.Y_AXIS));
+        cataloguePanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+
         catalogueBtn = new JButton("Catalogue Asset");
         catalogueBtn.addActionListener(e -> {
             System.out.println("Catalogue Asset Button Pressed");
         });
 
-        cataloguePanel.add(catalogueBtn);
+        cataloguePanel.add(dragArea, BorderLayout.CENTER);
+        cataloguePanel.add(catalogueBtn, BorderLayout.SOUTH);
+    }
 
-        this.add(cataloguePanel, BorderLayout.CENTER);
+    /**
+     * Creates a JTextArea that can be dragged and dropped files onto.
+     * 
+     * @see https://stackoverflow.com/questions/811248/how-can-i-use-drag-and-drop-in-swing-to-get-file-path
+     *      for how I got this to work
+     */
+    private void createDragArea() {
+        dragArea = new JTextPane();
+        dragArea.setText("Drag and drop files here");
+        
+        // formatting
+        dragArea.setFont(new Font("Serif", Font.BOLD, 30));
+        dragArea.setEditable(false);
+        dragArea.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        dragArea.setBounds(10, 10, 100, 100);
+        
+        // center text
+        SimpleAttributeSet center = new SimpleAttributeSet();
+        StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+        dragArea.getStyledDocument().setParagraphAttributes(0, dragArea.getText().length(), center, false);
+
+        dragArea.setDropTarget(new DropTarget() {
+        public synchronized void drop(DropTargetDropEvent evt) {
+        try {
+        evt.acceptDrop(DnDConstants.ACTION_COPY);
+        List<File> droppedFiles = (List<File>) evt.getTransferable()
+        .getTransferData(DataFlavor.javaFileListFlavor);
+        for (File file : droppedFiles) {
+        System.out.println(file.getAbsolutePath());
+        }
+        } catch (Exception ex) {
+        ex.printStackTrace();
+        }
+        }
+        });
     }
 }
