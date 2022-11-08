5e9ad2bf43d03823f3326b82af136d71c5cb98e4
Kiwifan1 <joshua.venable@outlook.com>
2022-11-07 16:19:26 -0800

    showing attributes works

---
+++ Data/testData.sql
@@ -0,0 +1,7 @@
+-- Inserts test data for project
+
+INSERT INTO Attribute
+VALUES
+    ('Goblin', 'A little green man'),
+    ('Orc', 'A large green man'),
+    ('Human', '');
--- UI/Home.java
+++ UI/Home.java
@@ -1,6 +1,9 @@
 package UI;
 
+import Logic.Attribute;
 import Logic.ConnectLogic;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
 
@@ -18,6 +21,7 @@ public class Home extends JFrame {
     public static int HEIGHT = 600;
 
     ConnectLogic logic;
+    Attribute attribute;
 
     JPanel homePanel;
     JPanel cataloguePanel;
@@ -26,11 +30,12 @@ public class Home extends JFrame {
     JTextPane dragArea;
 
     JButton catalogueBtn;
-    JScrollPane tagScroll;
+    JScrollPane attributeScroll;
 
     public Home() {
         super("Home");
-        // logic = new ConnectLogic();
+        logic = new ConnectLogic();
+        attribute = new Attribute(logic);
 
         // set up the frame
         setSize(WIDTH, HEIGHT);
@@ -50,40 +55,21 @@ public class Home extends JFrame {
         });
 
         // tag scroll pane
-        tagScroll = new JScrollPane();
+        attributeScroll = new JScrollPane();
 
-        // add checkboxes to the scroll pane
-        String[] tags = { "tag1", "tag2", "tag3", "tag4", "tag5", "tag6", "tag7", "tag8", "tag9", "tag10", "tag11",
-                "tag12",
-                "tag13", "tag14", "tag15", "tag16", "tag17", "tag18", "tag19", "tag20", "tag21", "tag22", "tag23",
-                "tag24",
-                "tag25", "tag26", "tag27", "tag28", "tag29", "tag30", "tag31", "tag32", "tag33", "tag34", "tag35",
-                "tag36",
-                "tag37", "tag38", "tag39", "tag40", "tag41", "tag42", "tag43", "tag44", "tag45", "tag46", "tag47",
-                "tag48",
-                "tag49", "tag50", "tag51", "tag52", "tag53", "tag54", "tag55", "tag56", "tag57", "tag58", "tag59",
-                "tag60",
-                "tag61", "tag62", "tag63", "tag64", "tag65", "tag66", "tag67", "tag68", "tag69", "tag70", "tag71",
-                "tag72",
-                "tag73", "tag74", "tag75", "tag76", "tag77", "tag78", "tag79", "tag80", "tag81", "tag82", "tag83",
-                "tag84",
-                "tag85", "tag86", "tag87", "tag88", "tag89", "tag90", "tag91", "tag92", "tag93", "tag94", "tag95",
-                "tag96",
-                "tag97", "tag98", "tag99", "tag100", "tag101", "tag102", "tag103", "tag104", "tag105", "tag106",
-                "tag107",
-                "tag108", "tag109", "tag110", "tag111", "tag112", "tag113", "tag114", "tag115", "tag116", "tag117",
-                "tag118", "tag119", "tag120", "tag121", "tag122", "tag123", "tag124", "tag125", "tag126", "tag127" };
+        // TODO: Add checkboxes to the scroll pane
+        ArrayList<String[]> attributes = attribute.getAllAttributes();
 
-        JPanel tagPanel = new JPanel();
-        tagPanel.setLayout(new BoxLayout(tagPanel, BoxLayout.Y_AXIS));
+        JPanel attributePanel = new JPanel();
+        attributePanel.setLayout(new BoxLayout(attributePanel, BoxLayout.Y_AXIS));
 
-        for (String tag : tags) {
-            JCheckBox tagBox = new JCheckBox(tag);
-            tagPanel.add(tagBox);
+        for (String[] tag : attributes) {
+            JCheckBox tagBox = new JCheckBox(tag[0]);
+            attributePanel.add(tagBox);
         }
 
-        tagScroll = new JScrollPane(tagPanel);
-        tagScroll.setPreferredSize(new Dimension(200, 200));
+        attributeScroll = new JScrollPane(attributePanel);
+        attributeScroll.setPreferredSize(new Dimension(200, 200));
     }
 
     private void makeHomePanel() {
@@ -105,7 +91,7 @@ public class Home extends JFrame {
 
         // create the spacing and add the buttons
         cataloguePanel.add(Box.createHorizontalGlue());
-        cataloguePanel.add(tagScroll);
+        cataloguePanel.add(attributeScroll);
         cataloguePanel.add(Box.createRigidArea(new Dimension(10, 0)));
         cataloguePanel.add(catalogueBtn);
     }
