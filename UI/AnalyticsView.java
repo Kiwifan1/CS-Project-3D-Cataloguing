45f905897ab4bbf1daf8a88d503c7178c7959c41
Kiwifan1 <joshua.venable@outlook.com>
2022-12-05 13:34:25 -0800

    Working on analytics

--- UI/AnalyticsView.java
+++ UI/AnalyticsView.java
@@ -31,47 +31,11 @@ public class AnalyticsView extends BoilerPlateView implements ActionListener {
 
         mainPanel = new JPanel();
 
-        createSidePanel();
-
         this.add(mainPanel);
 
         addMenuListeners();
 
         this.setVisible(true);
-    }
-
-    // analytics view will have a list of quantitative analysis items on the
-    // left-hand side
-    private void createSidePanel() {
-        // create side panel
-        JPanel sidePanel = new JPanel();
-        sidePanel.setLayout(new BoxLayout(sidePanel, BoxLayout.Y_AXIS));
-        sidePanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-        // add side panel to main panel
-        mainPanel.add(sidePanel, BorderLayout.WEST);
-    }
-
-    // in the center of the screen, there will be a display area for graphs
-    private void createDisplayArea() {
-        // create display area
-        JPanel displayArea = new JPanel();
-        displayArea.setLayout(new BoxLayout(displayArea, BoxLayout.Y_AXIS));
-        displayArea.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-        // add display area to main panel
-        mainPanel.add(displayArea, BorderLayout.CENTER);
-    }
-
-    // on the bottom, there will be an audit log
-    private void createAuditLog() {
-        // create audit log
-        JPanel auditLog = new JPanel();
-        auditLog.setLayout(new BoxLayout(auditLog, BoxLayout.Y_AXIS));
-        auditLog.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-        // add audit log to main panel
-        mainPanel.add(auditLog, BorderLayout.SOUTH);
     }
 
     @Override
